746150f9603e75b0615ef32d43162a16f39351c9
Alexander Albrecht <alexander.albrecht2004@gmail.com>
2025-08-13 11:53:57 +0200

    update routesname from folder logging in auth

--- config/routes.php
+++ config/routes.php
@@ -1,10 +1,10 @@
 <?php
 return [
     '/' => __DIR__ . '/../src/pages/dashboard.php',
-    '/login' =>  __DIR__ . '/../src/logging/log/log_Overlay.php',
-    '/login_controller' => __DIR__ . '/../src/logging/log/log_controller.php',
-    '/register' =>  __DIR__ . '/../src/logging/reg/reg_Overlay.php',
-    '/reg_controller' =>  __DIR__ . '/../src/logging/reg/reg_controller.php',
+    '/login' => __DIR__ . '/../src/auth/log/log_Overlay.php',
+    '/login_controller' => __DIR__ . '/../src/auth/log/log_controller.php',
+    '/register' => __DIR__ . '/../src/auth/reg/reg_Overlay.php',
+    '/reg_controller' => __DIR__ . '/../src/auth/reg/reg_controller.php',
     '/impressum' =>  __DIR__ . '/../src/pages/impressum.php',
-    '/user_exist' => __DIR__ . '/../src/logging/log/user_exist.php',
+    '/user_exist' => __DIR__ . '/../src/auth/log/user_exist.php',
     ];
--- src/pages/dashboard.php
+++ src/pages/dashboard.php
@@ -61,5 +61,5 @@ require __DIR__ . '/../../partials/header.php';
 <?php
 require __DIR__ . '/../../partials/footer.php';
 
-require_once __DIR__ . '/../logging/functions.php';
+require_once __DIR__ . '/../auth/functions.php';
 session_delete();
