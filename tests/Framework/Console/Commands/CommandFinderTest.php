5541bdac196c2f3f5cdec339c28a52be038895c2
Alexander Albrecht <alexander.albrecht2004@gmail.com>
2025-11-17 13:44:43 +0100

    CommandFinderTest hinzugefügt

--- tests/Framework/Console/Commands/CommandFinderTest.php
+++ tests/Framework/Console/Commands/CommandFinderTest.php
@@ -2,20 +2,22 @@
 
 namespace Test\Framework\Console\Commands;
 
+use Framework\Console\CommandFinder;
 use PHPUnit\Framework\TestCase;
 
 class CommandFinderTest extends TestCase
 {
     public function testGetClassName(): void
     {
+        $finder = new CommandFinder();
 
-    }
+        $path = __DIR__ . '/../../src/Console/MyCommand.php';
+        $directory = realpath(__DIR__ . '/../../src/Console');
+        $namespace = 'Framework\\Console';
 
-    public function testIsValidCommand(): void
-    {
-    }
+        $expected = 'App\\Console\\MyCommand';
+        $result = $finder->getClassName($path, $directory, $namespace);
 
-    public function testFind(): void
-    {
+        $this->assertNotEquals($expected, $result);
     }
 }
